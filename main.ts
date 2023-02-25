b407f80ca5f0e22ad6a318793b60f4144849695a
Lekono <70530844+Lekono22@users.noreply.github.com>
2023-02-25 17:44:58 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 basic.forever(function () {
-	
+    if (JoyCar.sonar() < 30) {
+        JoyCar.drive(FRDirection.Forward, 60)
+    } else {
+        if (JoyCar.sonar() > 30) {
+            JoyCar.stop(StopIntensity.Intense)
+            JoyCar.hazardlights(ToggleSwitch.On)
+        }
+    }
 })
