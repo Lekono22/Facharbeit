f019331667e898c5d56a3bb66279d3cfa6f22e5a
Lekono <70530844+Lekono22@users.noreply.github.com>
2023-02-27 09:51:12 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 basic.forever(function () {
-    if (JoyCar.sonar() < 30) {
+    if (JoyCar.sonar() > 30) {
         JoyCar.drive(FRDirection.Forward, 60)
     } else {
-        if (JoyCar.sonar() > 30) {
+        if (JoyCar.sonar() < 30) {
             JoyCar.stop(StopIntensity.Intense)
             JoyCar.hazardlights(ToggleSwitch.On)
         }
